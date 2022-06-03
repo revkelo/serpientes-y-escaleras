9a760809872e9828852f1c5c70b702344676f37a
revkelo <105946335+revkelo@users.noreply.github.com>
2022-06-03 18:02:38 -0500

    Delete ProyectoEYS directory

--- ProyectoEYS/src1/co/edu/unbosque/controlador/Main.java
+++
@@ -1,8 +0,0 @@
-package co.edu.unbosque.controlador;
-
-public class Main {
-	public static void main(String[] args) {
-		Controlador controlador = new Controlador();
-		controlador.ejecutar();
-	}
-}
